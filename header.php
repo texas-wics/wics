e3b5d5cd4a43381f4629014c15daf1453d81c3c6
Nikita Sharma <nikitasharma@utexas.edu>
2020-06-14 21:11:48 -0500

    initial template for 20-21 website

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+    echo "
+    <nav class=\"navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light\" id=\"ftco-navbar\">
+	    <div class=\"container\">
+	      <a class=\"navbar-brand\" href=\"index.html\">WiCS <span>UT Austin</span></a>
+	      <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#ftco-nav\" aria-controls=\"ftco-nav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+	        <span class=\"oi oi-menu\"></span> Menu
+	      </button>
+
+	      <div class=\"collapse navbar-collapse\" id=\"ftco-nav\">
+	        <ul class=\"navbar-nav ml-auto\">
+	          <li class=\"nav-item active\"><a href=\"index.html\" class=\"nav-link\">Home</a></li>
+	          <li class=\"nav-item\"><a href=\"about.html\" class=\"nav-link\">About</a></li>
+	          <li class=\"nav-item\"><a href=\"attorneys.html\" class=\"nav-link\">Programs</a></li>
+	          <li class=\"nav-item\"><a href=\"practice-areas.html\" class=\"nav-link\">Calendar</a></li>
+	          <li class=\"nav-item\"><a href=\"case.html\" class=\"nav-link\">Resources</a></li>
+	          <li class=\"nav-item\"><a href=\"blog.html\" class=\"nav-link\">Hackathon</a></li>
+	          <li class=\"nav-item\"><a href=\"contact.html\" class=\"nav-link\">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+      ";
+?>
